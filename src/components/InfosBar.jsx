eb24c2c03fdcbc077220497e79943a38decff74f
Rudy <rpalfray87@gmail.com>
2022-11-08 20:15:34 +0100

    Correction d'une définition et bug

--- src/components/InfosBar.jsx
+++ src/components/InfosBar.jsx
@@ -3,7 +3,7 @@ export default function InfosBar({score, reset}) {
         <div className="infosBar">
             <p>Score : {score[0]}/{score[1]}</p>
             <i>
-                <span onClick={reset} class="material-symbols-outlined icon-button">
+                <span onClick={reset} className="material-symbols-outlined icon-button">
                     restart_alt
                 </span>
             </i>
